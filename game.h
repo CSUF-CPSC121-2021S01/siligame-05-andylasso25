204474cd7b09fed55fc4754b09173258f03a1421
andylasso25 <andylasso25@yahoo.com>
2021-05-15 05:35:04 +0000

    Project 5 Submission

--- game.h
+++ game.h
@@ -13,11 +13,11 @@ class Game : public graphics::AnimationEventListener,
   Game(int width, int height) : game_screen_(width, height) {}
 
   graphics::Image& GetGameScreen() { return game_screen_; }
-  std::unique_ptr<Opponent> GetOpponents() { return opponents_; }
-  std::unique_ptr<OpponentProjectile> GetOpponentProjectiles() {
+  std::vector<Opponent>& GetOpponents() { return opponents_; }
+  std::vector<OpponentProjectile>& GetOpponentProjectiles() {
     return opponent_projectiles_;
   }
-  std::unique_ptr<PlayerProjectile> GetPlayerProjectiles() {
+  std::vector<PlayerProjectile>& GetPlayerProjectiles() {
     return player_projectiles_;
   }
 
@@ -45,9 +45,9 @@ class Game : public graphics::AnimationEventListener,
 
  private:
   graphics::Image game_screen_;
-  std::unique_ptr<Opponent> opponents_;
-  std::unique_ptr<OpponentProjectile> opponent_projectiles_;
-  std::unique_ptr<PlayerProjectile> player_projectiles_;
+  std::vector<Opponent> opponents_;
+  std::vector<OpponentProjectile> opponent_projectiles_;
+  std::vector<PlayerProjectile> player_projectiles_;
   Player my_player_;
 };
 
